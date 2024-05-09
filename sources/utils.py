8dc517d577d0f5b774c65f8a018e2e6a2bcc3aa4
XiXimun <ximun.loyatho@gmail.com>
2024-05-09 15:38:41 +0200

    add popularity

--- sources/utils.py
+++ sources/utils.py
@@ -47,8 +47,58 @@ def get_year_range(df):
 
 
 @st.cache_data
-def build_fig(df):
-    df = df.groupby(by=['prénom', 'année']).agg(nombre=("nombre", "sum")).reset_index()
-    fig = px.line(df, x="année", y="nombre", title='Naissances en France', color='prénom', markers=True)
-    fig.update_yaxes(rangemode="tozero")
+def get_stats(names_list, df_allnames):
+    df_allnames_grp = df_allnames.groupby(['prénom', 'année']).agg(nombre=('nombre', 'sum')).reset_index()
+
+    df = pd.DataFrame()
+    for i, df_allnames_iyear in df_allnames_grp.groupby('année'):
+        df_allnames_iyear['popularité (rang)'] = df_allnames_iyear['nombre'].rank(method='max', ascending=False).astype('int')
+        df_allnames_iyear['popularité (%)'] = 100*(1. - df_allnames_iyear['nombre'].rank(method='max', ascending=False, pct=True))
+        # dft = dft.sort_values(['nombre', 'prénom'], ascending=[False, True]).reset_index(drop=True)
+
+        df_iyear = df_allnames_iyear[df_allnames_iyear['prénom'].isin(names_list)]
+        df = pd.concat([df, df_iyear])
+    return df
+
+
+@st.cache_data
+def build_map(df):
+    france_departments = px.data.gapminder().query("country == 'France'")
+    fig = px.choropleth_mapbox(
+        france_departments,
+        geojson='https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/departements.geojson',
+        locations='iso_alpha',
+        featureidkey='properties.code',
+        color='lifeExp',
+        hover_name='country',
+        hover_data=['iso_alpha', 'lifeExp'],
+        color_continuous_scale="Viridis",
+        mapbox_style="carto-positron",
+        zoom=4,
+        center={"lat": 46.603354, "lon": 1.888334}
+    )
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+
+
+    from urllib.request import urlopen
+    import json
+    import numpy as np
+
+    with urlopen('https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/departements-version-simplifiee.geojson') as response:
+        geojson = json.load(response)
+    df = pd.DataFrame([x['properties'] for x in geojson['features']])
+    df['randNumCol'] = np.random.randint(0, 10, df.shape[0]).astype('str')
+
+    fig = px.choropleth_mapbox(df, geojson=geojson, featureidkey='properties.code', locations='code', 
+                            color="randNumCol", center = {"lat":47, "lon":2}, zoom=4.3, mapbox_style="carto-positron",
+                            opacity=0.5)
+
+    fig.update_layout(mapbox_style="open-street-map",
+                    showlegend=False,
+                    margin={"r":0,"t":0,"l":0,"b":0}, 
+                    width=600, 
+                    height=500
+                    )
+
     return fig
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-from sources.utils import get_names, get_df, get_departments, build_fig, get_year_range
+from sources.utils import get_names, get_df, get_departments, get_year_range, build_map, get_stats
 
 
 # ----------------- general parameters -----------------
@@ -10,8 +12,58 @@ names_file = './data/insee.fr/dpt2022.csv'
 # ------------------------------------------------------
 
 
-def home():
-    st.title(':baby: Tout savoir sur les prénoms !')
+def un_prenom():
+    st.title(':baby: Tout savoir sur un prénom !')
+    st.write("Vous trouverez ici toutes les statistiques des prénoms donnés en France recensés par l'INSEE.")
+
+    df_allnames = get_df(names_file)
+
+    sexes = st.multiselect(label='Fille ou garçon ?', options=['Fille', 'Garçon'])
+    sexes = [2 if x == 'Fille' else 1 for x in sexes]
+    df_allnames = df_allnames[df_allnames['sexe'].isin(sexes)]
+
+    names_list = get_names(df_allnames)
+    names_list = st.multiselect('Choisis des prénoms', names_list)
+    df_selnames = df_allnames[df_allnames['prénom'].isin(names_list)]
+
+    if names_list:
+
+        departments_list = get_departments(df_selnames)
+        departments_list = st.multiselect('Choisis une zone géographique', departments_list)
+        if departments_list:
+            df_selnames = df_selnames[df_selnames['département'].isin(departments_list)]
+            df_allnames = df_allnames[df_allnames['département'].isin(departments_list)]
+
+        year_min, year_max = get_year_range(df_selnames)
+        year_min, year_max = st.slider('Choisis une période', year_min, year_max, (year_min, year_max))
+        df_selnames = df_selnames[df_selnames['année'].isin(range(year_min, year_max + 1))]
+        df_allnames = df_allnames[df_allnames['année'].isin(range(year_min, year_max + 1))]
+
+        df = get_stats(names_list, df_allnames)
+
+        fig = px.line(df, x='année', y='nombre', title='Nombre de naissances', color='prénom', markers=True)
+        fig.update_yaxes(rangemode="tozero")
+        st.plotly_chart(fig, use_container_width=True)
+
+        type_pop = st.radio('Popularité :', ['rang', '%'], horizontal=True, captions=['Classement absolu', 'Classement relatif'])
+
+        fig = px.line(df, x="année", y=f"popularité ({type_pop})", title='Popularité', color='prénom', markers=True)
+        if type_pop == 'rang':
+            fig.update_yaxes(rangemode="tozero")
+            fig.update_yaxes(autorange="reversed")
+        elif type_pop == '%':
+            fig.update_layout(yaxis_range=[0, 100])
+        st.plotly_chart(fig, use_container_width=True)
+
+        # fig = build_map(df_selnames)
+        # st.plotly_chart(fig, use_container_width=True)
+
+    # st.dataframe(df_selnames.reset_index(drop=True))
+
+
+def prenoms_populaires():
+
+    st.title(':star2: Les prénoms populaires !')
     st.write("Vous trouverez ici toutes les statistiques des prénoms donnés en France recensés par l'INSEE.")
 
     df = get_df(names_file)
@@ -20,42 +72,36 @@ def home():
     sexes = [2 if x == 'Fille' else 1 for x in sexes]
     df = df[df['sexe'].isin(sexes)]
 
-    names_list = get_names(df)
-    names_list = st.multiselect('Choisis des prénoms', names_list)
-    df = df[df['prénom'].isin(names_list)]
+    departments_list = get_departments(df)
+    departments_list = st.multiselect('Choisis une zone géographique', departments_list)
+    if departments_list:
+        df = df[df['département'].isin(departments_list)]
 
-    if names_list:
-        departments_list = get_departments(df)
-        departments_list = st.multiselect('Choisis une zone géographique', departments_list)
-        if departments_list:
-            df = df[df['département'].isin(departments_list)]
+    year_min, year_max = get_year_range(df)
+    year_min, year_max = st.slider('Choisis une période', year_min, year_max, (year_min, year_max))
+    df = df[df['année'].isin(range(year_min, year_max + 1))]
 
-        year_min, year_max = get_year_range(df)
-        year_min, year_max = st.slider('Choisis une période', year_min, year_max, (year_min, year_max))
-        df = df[df['année'].isin(range(year_min, year_max + 1))]
-
-        fig = build_fig(df)
-        st.plotly_chart(fig, use_container_width=True)
-
-    # st.dataframe(df.reset_index(drop=True))
+    st.dataframe(df.reset_index(drop=True))
 
 
-def about():
+def a_propos():
     st.title('À propos')
     st.write("Les statistiques proviennent des données fournies par l'INSEE.")
     st.write("Vous retrouverez les prénoms donnés en France entre 1900 et 2022. Les prénoms donnés moins de 3 fois par an et par département ne sont pas reportés.")
     st.markdown('Toutes suggestions, remarques, bugs ou encouragements peuvent être transmis au webmaster: <a href="mailto:web.xilo@gmail.com">web.xilo@gmail.com</a>', unsafe_allow_html=True)
 
 
-
 def main():
     st.set_page_config(page_title='Prénoms', page_icon=':baby:')
-    page = st.sidebar.radio(" ", ["Accueil", "À propos"])
+    # page = st.sidebar.radio(' ', ['Un prénom', 'Prénoms populaires', 'À propos'])
+    page = st.sidebar.radio(' ', ['Un prénom', 'À propos'])
 
-    if page == "Accueil":
-        home()
+    if page == "Un prénom":
+        un_prenom()
+    if page == "Prénoms populaires":
+        prenoms_populaires()
     elif page == "À propos":
-        about()
+        a_propos()
 
 
 if __name__ == "__main__":
